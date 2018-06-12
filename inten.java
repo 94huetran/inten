706ee56996e193520132522576486db3687f7b6d
Tran Thu Hue <94huetran@gmail.com>
2018-06-12 13:06:23 +0700

    add inten file

---
+++ inten.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class inten {
+    public static void main(String[] args) {
+        System.out.println("Enter your name");
+        String name =Scanner.nextLine();
+        System.out.println("Hello:"+ name);
+
+    }
+}
